34afba52b784c1b6d7e38ba45f480b19a5f7f480
Miguel Angel Jimenez <miguel.js@gmail.com>
2018-03-11 18:41:43 +0100

    Add install flags

--- install/install.go
+++ install/install.go
@@ -2,21 +2,45 @@ package install
 
 import (
 	"fmt"
+    "flag"
 
 	"github.com/mitchellh/cli"
 )
 
 type Cmd struct {
 	ui cli.Ui
+
+    // flags
+    flags *flag.FlagSet
+    verbose bool
+    securize bool
+    principal string
 }
 
 func New(ui cli.Ui) *Cmd {
 	cmd := &Cmd{ui: ui}
+    cmd.init()
 	return cmd
 }
 
+func (c *Cmd) init() {
+    c.flags = flag.NewFlagSet("install", flag.ContinueOnError)
+    c.flags.BoolVar(&c.verbose, "v", false,
+        "Increase output info.")
+    c.flags.BoolVar(&c.securize, "securize", false,
+        "Securize cluster after install")
+    c.flags.StringVar(&c.principal, "principal", "root/admin",
+        "Principal to use when generating kerberos credentials")
+}
+
 func (c *Cmd) Run(args []string) int {
+    if err := c.flags.Parse(args); err != nil {
+        return 1
+    }
+
+    c.flags.VisitAll(func (flag *flag.Flag) { fmt.Printf("Visiting flag %v = %v\n", flag.Name, flag.Value) })
 	c.ui.Output(fmt.Sprintf("Running install command: eos install %v", args))
+
 	return 0
 }
 
